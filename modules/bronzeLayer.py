51f279ef0231c506fb670693e832d1adaf05c0c5
archan.phalanuphat@gmail.com <archan.phalanuphat@gmail.com>
2026-09-10 15:33:08 +0000

    Edit Bronze Class

--- modules/bronzeLayer.py
+++ modules/bronzeLayer.py
@@ -7,7 +7,7 @@ from abc import ABC, abstractmethod
 
 class IFileReaderStrategy(ABC):
     @abstractmethod
-    def get_dataframe(self) -> DataFrame:
+    def get_dataframe(self, file_path) -> DataFrame:
         pass
 
     
@@ -18,8 +18,7 @@ class CsvDataExtractor(IFileReaderStrategy):
     multiline: bool
     escape_option: str
     quote_option: str
-    file_path: str
-    def get_dataframe(self)-> DataFrame:
+    def get_dataframe(self, file_path)-> DataFrame:
         return(
             spark.read.format("csv")
             .option("header", self.header)
@@ -27,7 +26,7 @@ class CsvDataExtractor(IFileReaderStrategy):
             .option("quote", self.quote_option)
             .option("escape", self.escape_option)
             .option("multiLine", self.multiline)
-            .load(self.file_path)
+            .load(file_path)
         )
 
 class ExtractorFactory:
@@ -35,6 +34,8 @@ class ExtractorFactory:
     def create(strategy_type: str, **kwarges) -> IFileReaderStrategy:
         strategy_type = strategy_type.lower()
         if strategy_type == "csv":
+            print("csv")
+            print(**kwarges)
             return CsvDataExtractor(**kwarges)
         else:
             raise ValueError("")
@@ -53,27 +54,24 @@ class BronzeLayer:
         self.table_name_bronze = (f"{self.table_name}_bronze")
     
     def read_from_raw(self) -> DataFrame:
-        try:
-            reader = ExtractorFactory.create(self.file_format, **self.reader_options)
-            df_raw = reader.get_dataframe()
-            df_with_audit = (
-                df_raw
-                .withColumn("_load_dt", current_date())
-                .withColumn("_load_dttm", current_timestamp())
-                .withColumn("_file_name", col("_metadata.file_name"))
-                .withColumn("_file_path", col("_metadata.file_path"))
-                .withColumn("_file_size", col("_metadata.file_size"))
-                .withColumn("_file_mod", col("_metadata.file_modification_time"))
-            )
-            return df_with_audit
-        except:
-            raise ValueError("")
-
-    def write_file(self, df:DataFrame) -> bool:
-        (
-            df
-            .write
-            .format(self.format_data)
-            .mode(self.write_mode)
-            .saveAsTable(self.table_name_bronze)
+        reader = ExtractorFactory.create(self.file_format, **self.reader_options)
+        df_raw = reader.get_dataframe(self.file_path)
+        df_with_audit = (
+            df_raw
+            .withColumn("_load_dt", current_date())
+            .withColumn("_load_dttm", current_timestamp())
+            .withColumn("_file_name", col("_metadata.file_name"))
+            .withColumn("_file_path", col("_metadata.file_path"))
+            .withColumn("_file_size", col("_metadata.file_size"))
+            .withColumn("_file_mod", col("_metadata.file_modification_time"))
         )
+        return df_with_audit
+
+    def write_file(self, df:DataFrame) -> None :
+            (
+                df
+                .write
+                .format(self.format_data)
+                .mode(self.write_mode)
+                .saveAsTable(self.table_name_bronze)
+            )
